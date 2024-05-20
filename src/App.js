da9a937a9051ac9df62137dcf3ae930cf172036d
Daniel Lee <leechef2010@gmail.com>
2024-05-19 19:16:24 -0600

    implementation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { GanderReveal } from "./gander-reveal"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GanderReveal />
+    </div>
+  );
+}
+
+export default App;
