989a8c1a50eb2f5ab192f89c84228fc9613ab1f5
Daniel Lee <leecheef2010@gmail.com>
2025-03-02 11:33:27 -0700

    infinite scroll

--- src/recipes/home.js
+++ src/recipes/home.js
@@ -1,24 +1,75 @@
-import { memo } from "react"
+import { memo, useState } from "react"
 import { RecipeCard } from "./recipe-card"
+import "./home.css"
 
 export const Home = memo(() => {
-  // TODO: infinite scroll
+  const [recipesToShow, setRecipesToShow] = useState(10)
+  const allRecipes = [
+    <RecipeCard id="tsupales" />,
+    <RecipeCard id="chickenAndMethiFruit" />,
+    <RecipeCard id="steamedStrannaHaspers" />,
+    <RecipeCard id="chouta" />,
+    <RecipeCard id="rocksStew" />,
+    <RecipeCard id="cremlingCakes" />,
+    <RecipeCard id="fishStew" />,
+    <RecipeCard id="tastySnails" />,
+    <RecipeCard id="misirWat" />,
+    <RecipeCard id="porkBroth" />,
+    <RecipeCard id="azishSausages" />,
+    <RecipeCard id="berryCake" />,
+    <RecipeCard id="bluebarJam" />,
+    <RecipeCard id="candiedFruit" />,
+    <RecipeCard id="chickenAndFriedTenem" />,
+    <RecipeCard id="clemabread" />,
+    <RecipeCard id="coconutCustard" />,
+    <RecipeCard id="crabCurry" />,
+    <RecipeCard id="dinnerRolls" />,
+    <RecipeCard id="fishSoup" />,
+    <RecipeCard id="fluffyPancakes" />,
+    <RecipeCard id="fruitedRice" />,
+    <RecipeCard id="fruitSoup" />,
+    <RecipeCard id="frybread" />,
+    <RecipeCard id="ingoTea" />,
+    <RecipeCard id="kuma" />,
+    <RecipeCard id="lavisAndCrab" />,
+    <RecipeCard id="lavisCurry" />,
+    <RecipeCard id="lavisFlatbread" />,
+    <RecipeCard id="lavisFlatbreadPocket" />,
+    <RecipeCard id="lavisPastrySquares" />,
+    <RecipeCard id="mashedSweetPotatoes" />,
+    <RecipeCard id="nutsAndHoneyPastry" />,
+    <RecipeCard id="porkSkewers" />,
+    <RecipeCard id="redPorkCurry" />,
+    <RecipeCard id="seededPancakes" />,
+    <RecipeCard id="shiki" />,
+    <RecipeCard id="simberriesAndCreamPastry" />,
+    <RecipeCard id="sodaBiscuits" />,
+    <RecipeCard id="stagmAndTallew" />,
+    <RecipeCard id="strawberryJam" />,
+    <RecipeCard id="sugaredFlatbread" />,
+    <RecipeCard id="sugaredMashedBeans" />,
+    <RecipeCard id="sweetBeanDonutHoles" />,
+    <RecipeCard id="sweetBeanFlatbread" />,
+    <RecipeCard id="tallewFlatbread" />,
+    <RecipeCard id="thaylenBread" />,
+    <RecipeCard id="vegetableCurrySoup" />,
+    <RecipeCard id="vegetablePancakes" />,
+    <RecipeCard id="vegetablesWithBlackSauce" />
+  ]
+
+  const handleScroll = e => {
+    const bottom =
+      Math.abs(
+        e.target.scrollHeight - (e.target.scrollTop + e.target.clientHeight)
+      ) <= 1
+    if (bottom && recipesToShow < allRecipes.length) {
+      setRecipesToShow(recipesToShow + 10)
+    }
+  }
+
   return (
-    <div>
-      <RecipeCard id="tsupales" />
-      <RecipeCard id="chickenAndMethiFruit" />
-      <RecipeCard id="steamedStrannaHaspers" />
-      <RecipeCard id="chouta" />
-      <RecipeCard id="rocksStew" />
-      <RecipeCard id="cremlingCakes" />
-      <RecipeCard id="fishStew" />
-      <RecipeCard id="tastySnails" />
-      <RecipeCard id="misirWat" />
-      <RecipeCard id="porkBroth" />
-      <br />
-      <p>
-        See the <a href="/#/recipes/recipes">recipes page</a> for more
-      </p>
+    <div className="recipeScrollContainer" onScroll={handleScroll}>
+      {allRecipes.slice(0, recipesToShow)}
     </div>
   )
 })
