26d74bbe40e831cb5cd51e9da224d97244409b39
Daniel Lee <leecheef2010@gmail.com>
2025-04-08 21:03:06 -0600

    update mashed sweet potatoes recipe

--- src/recipes/recipes/mashedSweetPotatoes.js
+++ src/recipes/recipes/mashedSweetPotatoes.js
@@ -6,7 +6,7 @@ export const mashedSweetPotatoes = {
   summary: "Creamy and coconutty mashed purple sweet potatoes",
   inspiration: null,
   quantity: "4 servings",
-  time: "45 minutes",
+  time: "30 minutes",
   notes:
     "Adapting this recipe was fairly straightforward. The biggest question was what liquid and fat to add to the mash to help with texture, since I didn't want to use milk as is normal on earth. Coconut milk worked well for that. It brings a strong flavor to the dish, but I find that it works well with the sweet potatoes.",
   quotes:
@@ -39,20 +39,20 @@ export const mashedSweetPotatoes = {
         },
         {
           item: "coconut",
-          display: "1/2 cup coconut milk"
+          display: "1/2 cup coconut milk, plus additional"
         },
         {
           item: "salt",
-          display: "3/4 tsp salt"
+          display: "1 tsp salt"
         }
       ]
     }
   ],
   steps: [
     "Fill a pot half full of water and bring to a boil over high heat. Peel and chop the sweet potatoes. Add to the water and boil until soft.",
-    "Dice the onion. Peel and mince the garlic and ginger. Heat the coconut oil in a pan over medium heat. Add the onions, garlic, and ginger to the pan and saute until the onions are mostly transparent but not yet browning.",
+    "Dice the onion. Peel and mince the garlic and ginger. Heat the coconut oil in a pan over medium heat. Add the onions, garlic, and ginger to the pan and saute until the onions are mostly translucent but not yet browning.",
     "Add the coconut milk and salt to the pan. Cook and stir until heated through.",
     "Once the sweet potatoes are cooked, drain the water. Add the coconut milk mixture to the potatoes, then mash thoroughly.",
-    "Add additional coconut milk a little at a time, mixing well, until it reaches a consistency that you like. I prefer it thick, so I only add a tablespoon or two. Serve warm."
+    "Add additional coconut milk a little at a time, mixing well, until it reaches a consistency that you like. I prefer it thick, so I only add couple tablespoons. Serve warm."
   ]
 }
